da3033f34d60fe9c23e764224e815e8ebc82fc50
Nico Smith <web@niiccoo2.xyz>
2025-08-15 08:57:48 -0400

    Change to only allow 10 char input

--- src/main.cpp
+++ src/main.cpp
@@ -11,24 +11,30 @@ void setup() {
     M5Cardputer.Display.setTextColor(GREEN);
     M5Cardputer.Display.setTextDatum(middle_center);
     M5Cardputer.Display.setFont(&fonts::Orbitron_Light_24);
-    M5Cardputer.Display.setTextSize(1);
+    M5Cardputer.Display.setTextSize(0.75);
     M5Cardputer.Display.drawString("Cardputer Calculator",
                                    M5Cardputer.Display.width() / 2,
                                    M5Cardputer.Display.height() / 2);
+    M5Cardputer.Display.setTextSize(1); // Set the text back to 1 after drawing ^^^
 }
+
+
 
 String equation = "";
 
 void loop() {
     M5Cardputer.update();
     
+    
     if (M5Cardputer.Keyboard.isChange()) {
         if (M5Cardputer.Keyboard.isPressed()) {
             Keyboard_Class::KeysState status = M5Cardputer.Keyboard.keysState();
           
             for (auto i : status.word) {
-                if (equation != "") {
-                    equation = equation + i;
+                if (equation.length() >= 10) { // Use >= becuase it checks BEFORE it adds a new char
+                  equation = equation; // Do nothing so max input is 10
+                } else if (equation != "") {
+                  equation = equation + i;
                 } else {
                     equation += i;
                 }
